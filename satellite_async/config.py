c4865da8384babde7d2ce0f5b843432778741672
jesus <jmlopezfabian@gmail.com>
2025-07-27 13:30:54 -0600

    Working with relative paths

--- satellite_async/config.py
+++ satellite_async/config.py
@@ -2,6 +2,6 @@ import os
 
 BASE_URL = "https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/5000/VNP46A1/{year}/{day}/"
 IMAGE_PATH = "HDFEOS/GRIDS/VNP_Grid_DNB/Data Fields/DNB_At_Sensor_Radiance_500m"
-PIXELES_MUNICIPIOS = "/home/gsu/ESCOM/Trabajo_Terminal/Data/municipios_coordenadas_pixeles.json"
+PIXELES_MUNICIPIOS = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'Data', 'municipios_coordenadas_pixeles.json')
 TOKEN = os.getenv("NASA_API_TOKEN")
 HEADERS = {"Authorization": f"Bearer {TOKEN}"} 
