982967db994297f6cad00b5841e8f831fa8be986
jmlopezfabian <jmlopezfabian@gmail.com>
2026-03-16 15:09:37 -0600

    updated base url

--- satellite_async/config.py
+++ satellite_async/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BASE_URL = "https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/5000/VNP46A1/{year}/{day}/"
+BASE_URL = "https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/5200/VNP46A1/{year}/{day}/"
 IMAGE_PATH = "HDFEOS/GRIDS/VNP_Grid_DNB/Data Fields/DNB_At_Sensor_Radiance_500m"
 PIXELES_MUNICIPIOS = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'municipios_coordenadas_pixeles.json')
 TOKEN = os.getenv("NASA_API_TOKEN")
